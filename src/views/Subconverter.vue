201f8516380c9fd7b23e201275bc75b24aedc4b4
sh2yz <91233882+sh2yz@users.noreply.github.com>
2022-08-28 10:28:37 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -6,6 +6,11 @@
           <div slot="header" style="blackground-color:#0F4677;text-align:center;font-size :25px !important;font-weight: bold !important;">
             <svg-icon icon-class="lock" style="margin-left: 20px" title="完整魔改版:v1.4"/>
             つつの订阅转换
+	    </div>
+          <el-container>
+            <el-form :model="form" label-width="80px" label-position="left" style="width: 100%;">
+              <el-form-item label="进阶选项:">
+	      </div>
             
          
 
